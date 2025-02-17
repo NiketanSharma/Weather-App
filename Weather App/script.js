e6a909807096e1fa2c5d4f4fee6fbd609825d903
NiketanSharma <149008719+NiketanSharma@users.noreply.github.com>
2025-02-17 23:49:07 +0530

    script.js

--- Weather App/script.js
+++ Weather App/script.js
@@ -5,7 +5,6 @@ const apiUrl="#";
 const searchBox=document.querySelector(".search input");
 const searchBtn=document.querySelector(".search button");
 const weatherIcon=document.querySelector(".weather-icon");
-
 async function checkWeather(city) { 
     const response=await fetch(apiUrl +city+ `&appid=${apiKey}`);
 
